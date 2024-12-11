f863f6c0881b610d3d871b83bbd98b7c7f8c9c61
TSOlami <tijani.saheed@yahoo.com>
2024-12-11 16:45:51 +0100

    Add utility functions for file downloading and timestamped filenames

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,26 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const getTimedFilename = (prefix: string, extension: string) => {
+  const timestamp = new Date().getTime();
+  return `${prefix}-${timestamp}.${extension}`;
+};
+
+export function downloadFile(
+  url: string,
+  options: { filename?: string; target?: string }
+) {
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = options.filename || "file";
+  if (options.target) {
+    a.target = options.target;
+  }
+  a.style.display = "none";
+  document.body.appendChild(a);
+  a.click();
+
+  window.URL.revokeObjectURL(url);
+  document.body.removeChild(a);
+}
